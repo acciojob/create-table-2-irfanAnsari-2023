912b2e2aaf5f1f8e2782cf364b6195fd13bbc73f
Irfan Ansari <123730480+irfanAnsari-2023@users.noreply.github.com>
2025-03-04 10:57:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 function createTable() {
     //Write your code here
+	let rn = prompt("Input number of rows");
+	let cn = prompt("Input number of columns);
+	//  parsing the inputs
+		rn = parseInt(rn,10);
+		cn = parseInt(cn,10);
+	// Validate input
+    if (isNaN(rows) || isNaN(cols)) {
+        alert("Please enter valid numeric values.");
+        return;
+	if(rn <= 0 || col <= 0){
+		alert("Number of rows and columns must be greater than 0.");
+		return;
+	}
+
+	let table = document.getElementById("myTable");
+	table.innerHTML = "";
+		for(let i= 0; i< rn; i++){
+			let tr = document.createElement("tr");
+			for(let j=0; j<cn; j++){
+				let td = document.createElement("td");
+				td.textContent = `Row-${i} Col-${j}`;
+				tr.appendChild(td);
+			}
+			table.appendChild(tr);
+		}
+		
   
 }
